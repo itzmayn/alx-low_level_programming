31c4c87ce0f871ea204394a033d0685df2b00ec0
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-03-22 17:24:48 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,43 +1,16 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10 == 0 && i > 0)
-        {
-            printf("\n");
-        }
-        printf("%02x ", (unsigned char)buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    char buffer[98] = {0};
+	int i;
 
-    simple_print_buffer(buffer, 98);
-    memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);
-    return (0);
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
