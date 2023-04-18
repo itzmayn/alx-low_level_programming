b7478120aa71849d46da19c4ea88855372e7fe24
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-04-18 13:49:58 +0000

    main

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,33 +8,25 @@
 * Return: NULL in case of error, pointer to allocated
 * space
 */
-
-char* _strdup(char* str)
+char *_strdup(char *str)
 {
-    char* cpy;
-    int index, len;
+	char *cpy;
+	int index, len;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    // Calculate the length of the input string
-    for (index = 0; str[index]; index++)
-        len++;
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
 
-    // Allocate memory for the duplicate string with extra padding
-    cpy = malloc(sizeof(char) * (2 * len + 1));
+	if (cpy == NULL)
+		return (NULL);
 
-    if (cpy == NULL)
-        return (NULL);
+	for (index = 0; str[index]; index++)
+		cpy[index] = str[index];
 
-    // Copy the input string to the duplicate string with some modifications
-    for (index = 0; str[index]; index++) {
-        // Insert a space character after every character in the duplicate string
-        cpy[2 * index] = str[index];
-        cpy[2 * index + 1] = ' ';
-    }
+	cpy[len] = '\0';
 
-    cpy[2 * len] = '\0'; // Null-terminate the duplicate string
-
-    return (cpy);
+	return (cpy);
 }
