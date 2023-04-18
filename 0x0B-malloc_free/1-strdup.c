70b5eea880eb14b5993e13aec13a1c25b7932083
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-04-18 14:08:31 +0000

    main

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,32 +1,37 @@
-#include "main.h";
+#include "main.h"
 
 /**
  * _strdup - returns a pointer to a newly allocated
- *           memory block containing a duplicate of the input string.
- * @str: Pointer to the input string to be duplicated.
+ * space in memory, which contains a copy of the
+ * string given as a parameter.
+ * @str: String to be copied
  *
- * Return: Pointer to the duplicate string, or NULL if memory allocation fails
+ * Return: NULL in case of error, pointer to allocated
+ * space
  */
-char* _strdup(char* str)
+char *_strdup(char *str)
 {
-    char* cpy;    /* Pointer to the duplicate string */
-    int index, len;    /* Variables for loop index and length of the input string */
+	char *cpy;
+	int index, len;
 
-    if (str == NULL)
-        return (NULL);    /* If input string is NULL, return NULL indicating error */
+	if (str == NULL) /* Check if input string is NULL */
+		return (NULL);
 
-    for (index = 0; str[index]; index++)
-        len++;    /* Calculate the length of the input string by counting characters */
+	/* Calculate length of input string */
+	for (index = 0; str[index]; index++)
+		len++;
 
-    cpy = malloc(sizeof(char) * (len + 1));    /* Allocate memory for the duplicate string */
+	/* Allocate memory for copy of input string */
+	cpy = malloc(sizeof(char) * (len + 1));
 
-    if (cpy == NULL)
-        return (NULL);    /* If memory allocation fails, return NULL indicating error */
+	if (cpy == NULL) /* Check if memory allocation failed */
+		return (NULL);
 
-    for (index = 0; str[index]; index++)
-        cpy[index] = str[index];    /* Copy characters from input string to the duplicate string */
+	/* Copy input string to newly allocated memory */
+	for (index = 0; str[index]; index++)
+		cpy[index] = str[index];
 
-    cpy[len] = '\0';    /* Null-terminate the duplicate string */
+	cpy[len] = '\0'; /* Add null terminator at the end of copied string */
 
-    return (cpy);    /* Return the pointer to the duplicate string */
+	return (cpy);
 }
