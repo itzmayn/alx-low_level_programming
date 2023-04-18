9df91916aed0b9d255527ec4ca53d91a292cf725
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-04-18 15:11:19 +0000

    m

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 upon successful execution
+ */
+int main(void)
+{
+    printf("%s\n", __FILE__);
+    return (0);
+}
