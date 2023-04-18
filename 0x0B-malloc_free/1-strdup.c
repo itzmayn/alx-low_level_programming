37f490950e9902726cf0df29c504f4ef1204ceca
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-04-18 14:03:13 +0000

    main

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,29 +1,32 @@
+#include "main.h";
+
 /**
- * _strdup - Returns a pointer to a newly allocated duplicate string.
- * @str: Pointer to the input string.
+ * _strdup - returns a pointer to a newly allocated
+ *           memory block containing a duplicate of the input string.
+ * @str: Pointer to the input string to be duplicated.
  *
- * Return: Pointer to the duplicate string, or NULL on error.
+ * Return: Pointer to the duplicate string, or NULL if memory allocation fails
  */
-char *_strdup(char *str)
+char* _strdup(char* str)
 {
-	char *cpy; /* Pointer to the duplicate string */
-	int i, len; /* Variables for loop index and length of the input string */
+    char* cpy;    /* Pointer to the duplicate string */
+    int index, len;    /* Variables for loop index and length of the input string */
 
-	if (str == NULL)
-		return (NULL); /* If input string is NULL, return NULL indicating error */
+    if (str == NULL)
+        return (NULL);    /* If input string is NULL, return NULL indicating error */
 
-	for (i = 0; str[i]; i++)
-		len++; /* Calculate the length of the input string by counting characters */
+    for (index = 0; str[index]; index++)
+        len++;    /* Calculate the length of the input string by counting characters */
 
-	cpy = malloc(sizeof(char) * (len + 1)); /* Allocate memory for the duplicate string */
+    cpy = malloc(sizeof(char) * (len + 1));    /* Allocate memory for the duplicate string */
 
-	if (cpy == NULL)
-		return (NULL); /* If memory allocation fails, return NULL indicating error */
+    if (cpy == NULL)
+        return (NULL);    /* If memory allocation fails, return NULL indicating error */
 
-	for (i = 0; str[i]; i++)
-		cpy[i] = str[i]; /* Copy characters from input string to the duplicate string */
+    for (index = 0; str[index]; index++)
+        cpy[index] = str[index];    /* Copy characters from input string to the duplicate string */
 
-	cpy[len] = '\0'; /* Null-terminate the duplicate string */
+    cpy[len] = '\0';    /* Null-terminate the duplicate string */
 
-	return (cpy); /* Return the pointer to the duplicate string */
+    return (cpy);    /* Return the pointer to the duplicate string */
 }
