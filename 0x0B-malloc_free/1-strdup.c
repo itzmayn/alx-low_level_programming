90fc28e7c6aace5bab4fb2e2cfa3caa335434b05
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-04-18 13:51:36 +0000

    main

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,25 +8,26 @@
 * Return: NULL in case of error, pointer to allocated
 * space
 */
-char *_strdup(char *str)
+char* _strdup(char* str)
 {
-	char *cpy;
-	int index, len;
+	char* cpy;    // Pointer to the duplicate string
+	int index, len;    // Variables for loop index and length of the input string
 
 	if (str == NULL)
-		return (NULL);
+		return (NULL);    // If input string is NULL, return NULL indicating error
 
 	for (index = 0; str[index]; index++)
-		len++;
-	cpy = malloc(sizeof(char) * (len + 1));
+		len++;    // Calculate the length of the input string by counting characters
+
+	cpy = malloc(sizeof(char) * (len + 1));    // Allocate memory for the duplicate string
 
 	if (cpy == NULL)
-		return (NULL);
+		return (NULL);    // If memory allocation fails, return NULL indicating error
 
 	for (index = 0; str[index]; index++)
-		cpy[index] = str[index];
+		cpy[index] = str[index];    // Copy characters from input string to the duplicate string
 
-	cpy[len] = '\0';
+	cpy[len] = '\0';    // Null-terminate the duplicate string
 
-	return (cpy);
+	return (cpy);    // Return the pointer to the duplicate string
 }
