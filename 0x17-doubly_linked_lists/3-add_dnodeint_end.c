629a07cecf48630aa677fc743f36690fa45594a1
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-06-15 18:05:34 +0000

    m

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,35 +1,19 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Adds a node at the end of a doubly linked list
- * @head: Pointer to a pointer to the head node
- * @n: The data integer for the new node
- * Return: Pointer to the newly added node
+ * dlistint_len - Returns the number of elements in a doubly linked list
+ * @h: Pointer to the head of the list
+ * Return: The number of nodes in the list
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+size_t dlistint_len(const dlistint_t *h)
 {
-    dlistint_t *new_node, *last_node;
+	size_t count = 0;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
 
-    new_node->n = n;
-    new_node->next = NULL;
-    new_node->prev = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
-
-    last_node = *head;
-    while (last_node->next != NULL)
-        last_node = last_node->next;
-
-    last_node->next = new_node;
-    new_node->prev = last_node;
-
-    return (new_node);
+	return count;
 }
