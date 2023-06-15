1dc925df199b8b32d61cf3636584a69f5c3b8ace
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-06-15 18:00:50 +0000

    m

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,18 +2,18 @@
 
 /**
  * dlistint_len - Returns the number of elements in a doubly linked list
- * @h: Pointer to the head node of the list
- * Return: The number of elements in the list
+ * @h: Pointer to the head of the list
+ * Return: The number of nodes in the list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    int count_node = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;    // Move to the next node
-        count_node++;   // Increment the node count
-    }
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
 
-    return (count_node);
+	return count;
 }
